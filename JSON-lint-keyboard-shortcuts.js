2f95d5e4037bc56a4cab416ff1abc5e1d0de57e0
Bill <41483625+mapsedge@users.noreply.github.com>
2025-06-05 15:21:55 -0500

    Create JSON-lint-keyboard-shortcuts.js

---
+++ JSON-lint-keyboard-shortcuts.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         JSON Lint keyboard shortcuts
+// @namespace    http://tampermonkey.net/
+// @version      2025-06-05
+// @description  try to take over the world!
+// @author       Bill in Kansas City MO
+// @match        https://jsonlint.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jsonlint.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+
+	window.addEventListener("keydown", function (event) {
+		// Check if the key pressed is F9
+		if (event.key === "F9") {
+			// Run your function here
+			myFunction();
+		}
+	});
+
+	function myFunction() {
+		let button2 = document.querySelector(".primary-button");
+		// Dispatch the event on the element
+		button2.click();
+	}
+})();
